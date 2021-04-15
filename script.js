29497f196b77b133a130c5a271184c6a1415d53f
Alex L <56426570+alexlawn@users.noreply.github.com>
2021-04-15 22:08:47 +0100

    Used localStorage to save user theme preference

--- script.js
+++ script.js
@@ -9,18 +9,32 @@ const darkStyle = "styles-dark.css";
 // Select the stylesheet <link>
 const theme = document.getElementById("theme");
 
+function lightFunction() {
+  theme.href = lightStyle;
+  // different sun for dark theme
+  document.getElementById("light").src="sun - light.png";
+  document.querySelector(".favicon").href = "favicon-light.ico";
+  window.localStorage.setItem('theme', 'light');
+}
+
+function darkFunction() {
+  theme.href = darkStyle;
+  // different sun for dark theme
+  document.getElementById("light").src="sun.jpg";
+  document.querySelector(".favicon").href = "favicon.ico";
+  window.localStorage.setItem('theme', 'dark');
+}
+
 // Listen for a click on the sun button (buttonLight)
-buttonLight.addEventListener("click", function() {
-    theme.href = lightStyle;
-    // different sun for light theme
-    document.getElementById("light").src = "sun - light.png";
-    document.querySelector(".favicon").href = "favicon-light.ico";
-  });
+buttonLight.addEventListener("click", lightFunction);
 
 // Listen for a click on the moon button (buttonDark)
-buttonDark.addEventListener("click", function() {
-    theme.href = darkStyle;
-    // different sun for dark theme
-    document.getElementById("light").src="sun.jpg";
-    document.querySelector(".favicon").href = "favicon.ico";
-  });
+buttonDark.addEventListener("click", darkFunction);
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  darkFunction();
+} else {
+  lightFunction();
+}
